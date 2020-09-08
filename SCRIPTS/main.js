65a6b4ca2b40284f3f62027384bb9e28666e9ef2
Coffey May <jcoffeymay6@gmail.com>
2020-09-08 18:55:27 -0500

    php trick for heroku

--- SCRIPTS/main.js
+++ SCRIPTS/main.js
@@ -1,35 +1,38 @@
-import { dotenv } from "../node_modules/dotenv";
+// import { dotenv } from "../node_modules/dotenv";
 
-console.log("secret message")
+// console.log("secret message")
 // process.env.API_KEY;
 
-require('dotenv').config();
+// require('dotenv').config();
+// const api_key = process.env.API_KEY;
+
+
 // const api_key = "adced04f1649ca9bd53f6267f3bd4317";
-const api_key = process.env.API_KEY;
 
-function fetchData() {
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=Nashville&appid=${api_key}`).then(response => {
-        return response.json()
+// function fetchData() {
 
-    }).then(data => {
-        console.log(data)
-        document.getElementById('weather_card').innerHTML = `<h4>${data.name}</h4>
-        
-        <h6>TN</h6>
-        <h5>icon${data.weather[0].icon}</h5>
-        <h6>${data.main.temp} °F</h6>
-        <label for="city-select">Choose City:</label>
+//     fetch(`http://api.openweathermap.org/data/2.5/weather?q=Nashville&appid=${api_key}`).then(response => {
+//         return response.json()
 
-        <select style="background-color: transparent;" name="cars" id="cars">
-            <option style="background-color: transparent;" value="London">London</option>
-            <option style="background-color: transparent;" value="New York">New York</option>
-            <option style="background-color: transparent;" value="Los Angeles">Los Angeles</option>
-            <option style="background-color: transparent;" value="Dallas">Dallas</option>
-        </select>`
-    })
-}
-fetchData()
+//     }).then(data => {
+//         console.log(data)
+//         document.getElementById('weather_card').innerHTML = `<h4>${data.name}</h4>
+
+//         <h6>TN</h6>
+//         <h5>icon${data.weather[0].icon}</h5>
+//         <h6>${data.main.temp} °F</h6>
+//         <label for="city-select">Choose City:</label>
+
+//         <select style="background-color: transparent;" name="cars" id="cars">
+//             <option style="background-color: transparent;" value="London">London</option>
+//             <option style="background-color: transparent;" value="New York">New York</option>
+//             <option style="background-color: transparent;" value="Los Angeles">Los Angeles</option>
+//             <option style="background-color: transparent;" value="Dallas">Dallas</option>
+//         </select>`
+//     })
+// }
+// fetchData()
 
 
 
